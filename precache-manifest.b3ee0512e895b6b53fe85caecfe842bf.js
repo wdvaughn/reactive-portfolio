cec7a5e80d12e05aed09b5a0929f58278b5428c1
William Vaughn <will.d.vaughn@gmail.com>
2020-03-28 14:37:15 -0400

    Updates

---
+++ precache-manifest.b3ee0512e895b6b53fe85caecfe842bf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baf911fa35b75f3cc9f6817f13290520",
+    "url": "/reactive-portfolio/index.html"
+  },
+  {
+    "revision": "5db62ab7ccb86bb5cf7f",
+    "url": "/reactive-portfolio/static/css/main.f95cb452.chunk.css"
+  },
+  {
+    "revision": "0858141bffaf1ebdc6e7",
+    "url": "/reactive-portfolio/static/js/2.2ddd399f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/reactive-portfolio/static/js/2.2ddd399f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5db62ab7ccb86bb5cf7f",
+    "url": "/reactive-portfolio/static/js/main.e1feec09.chunk.js"
+  },
+  {
+    "revision": "15078454e7e8a1f5185b",
+    "url": "/reactive-portfolio/static/js/runtime-main.0f1162cd.js"
+  }
+]);
