4ba655227ea7ad84695427c010164abd0c4bdf4c
William Vaughn <will.d.vaughn@gmail.com>
2020-03-28 14:50:09 -0400

    Updates

--- precache-manifest.0cf519d12f6eb22401435359c0aef873.js
+++ precache-manifest.0cf519d12f6eb22401435359c0aef873.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "baf911fa35b75f3cc9f6817f13290520",
+    "revision": "a4bfcf15ca8502c5e9ba58c1810213a1",
     "url": "/reactive-portfolio/index.html"
   },
   {
-    "revision": "5db62ab7ccb86bb5cf7f",
+    "revision": "8daf5f7ee5c16b8a30cc",
     "url": "/reactive-portfolio/static/css/main.f95cb452.chunk.css"
   },
   {
-    "revision": "0858141bffaf1ebdc6e7",
-    "url": "/reactive-portfolio/static/js/2.2ddd399f.chunk.js"
+    "revision": "bb79d92e92ea8afb9af8",
+    "url": "/reactive-portfolio/static/js/2.e3cf6e66.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/reactive-portfolio/static/js/2.2ddd399f.chunk.js.LICENSE.txt"
+    "url": "/reactive-portfolio/static/js/2.e3cf6e66.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5db62ab7ccb86bb5cf7f",
-    "url": "/reactive-portfolio/static/js/main.e1feec09.chunk.js"
+    "revision": "8daf5f7ee5c16b8a30cc",
+    "url": "/reactive-portfolio/static/js/main.2b6f7267.chunk.js"
   },
   {
     "revision": "15078454e7e8a1f5185b",
