cd508c606477171761e53785dd68abeb10596185
William Vaughn <will.d.vaughn@gmail.com>
2020-03-28 14:47:26 -0400

    changed a tag to Links

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <nav className="navbar navbar-light bg-light d-flex">
       <h1 className="navbar-brand text-light mr-auto px-3 logo">William Vaughn</h1>
-      <a className="px-3" href="/">About</a>
-      <a className="px-3" href="/contact">Contact</a>
-      <a className="px-3" href="/portfolio">Portfolio</a>
+      <Link className="px-3" to="/">About</Link>
+      <Link className="px-3" to="/contact">Contact</Link>
+      <Link className="px-3" to="/portfolio">Portfolio</Link>
     </nav>
   );
 }
